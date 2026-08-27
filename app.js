47dc53106587d1078b6f0893005f278de8a29373
Joy-karthik <75682653+Joy-karthik@users.noreply.github.com>
2026-08-27 18:01:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ var morgan      = require('morgan');
 
 var app = express();
 var server = require('http').Server(app);
-port = process.env.PORT || 9999;
+port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send("Hello World!!! Node Sample App is up and running!");
-});
 
 app.get('/ping', (req, res) => {
     res.send("Pong!");
